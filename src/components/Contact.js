46d147cee542aa074a2820ca4336622bd5bb001e
tfilewic <tfilewic@asu.edu>
2025-07-25 00:02:30 -0600

    Complete contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,15 @@
+import gmailIcon from '../assets/email.png'
+import githubIcon from '../assets/github-white.png'
+
 function Contact() {
   return (
-    <div>
-        Contact  Contact  Contact   Contact 
+    <div style={{ display: 'flex', justifyContent: 'center', gap: '3rem'}}>
+        <a href="mailto:tylerfilewich@gmail.com"> 
+          <img src={gmailIcon} alt="GitHub" style={{ width: '30vw', maxWidth: '90px',  transition: 'transform 0.2s ease' }} />
+        </a>
+        <a href="https://www.github.com/tfilewic">
+          <img src={githubIcon} alt="GitHub" style={{ width: '30vw', maxWidth: '90px',  transition: 'transform 0.2s ease' }} />
+        </a>
     </div>
   );
 }
